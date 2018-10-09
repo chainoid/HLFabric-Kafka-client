ad979c674a92fbe0f345cd56b918d2c74ca7f5cb
Chainoid Team <chainoid@gmx.com>
2018-10-09 11:27:36 +0300

    Removed unused client's files

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 # to be executed on start
 docker exec -it cli peer chaincode install -n postap -p github.com/chaincode -v v0.1
-docker exec -it cli peer chaincode instantiate -o orderer0.example.com:7050 -C posta-channel -n postap github.com/chaincode -v v0.1 -c '{"Args": []}'
+docker exec -it cli peer chaincode instantiate -o orderer0.example.com:7050 -C posta-channel -n postap github.com/chaincode -v v0.1 -c '{"Args":["initLedger"]}'
