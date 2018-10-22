a95bca9502aa96bfcef584fbcd9c81ea80519d9a
Chainoid Team <chainoid@gmx.com>
2018-10-22 22:10:00 +0300

    Update the working chaincode on the fly.

--- update.sh
+++ update.sh
@@ -10,4 +10,4 @@ rv="v$(shuf -i 200-10000 -n 1)"
 
 docker exec -it cli peer chaincode install -n postap -p github.com/chaincode -v "$rv"
 
-docker exec -it cli peer chaincode upgrade -o orderer.example.com:7050 -C $CHANNEL_NAME -n postap github.com/chaincode -v "$rv" -c '{"Args": ["queryAllParsels"]}'
+docker exec -it cli peer chaincode upgrade -o orderer0.example.com:7050 -C $CHANNEL_NAME -n postap github.com/chaincode -v "$rv" -c '{"Args": ["queryAllParsels"]}'
