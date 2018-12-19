313b8cc1af788b83505a42eed1293662677c256a
Chainoid Team <chainoid@gmx.com>
2018-12-19 22:31:42 +0200

    Change loglevel var to FABRIC_LOGGING_SPEC.

--- test.sh
+++ test.sh
@@ -5,6 +5,8 @@ set -ev
 # # # Init Ledger
 docker exec -it cli peer chaincode invoke -o orderer1.example.com:7050 -n postap -c '{"Args":["initLedger"]}' -C posta-channel
 
+sleep 2
+
 # # # Query all parsels 
 docker exec -it cli peer chaincode invoke -o orderer2.example.com:7050 -n postap -c '{"Args":["queryAllParsels"]}' -C posta-channel
 
